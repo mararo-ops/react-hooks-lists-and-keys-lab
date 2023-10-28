167c9fec56b06f09392d6aa02223385a75e439a1
mararo-ops <danielmararo1@gmail.com>
2023-10-28 22:43:06 +0300

    com[plete lab

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,20 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
-function ProjectList({ projects }) {
+function ProjectList({ projects }) { // receiving a prop of objects to map through
   console.log(projects);
+  const ProjectEach =projects.map((proj)=>{
+    return( // after mapping passing down the projects to project item to be shown there
+    <ProjectItem key={proj.id} technologies={proj.technologies} name={proj.name} about={proj.about}/>
+    )
+  })
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {ProjectEach}
+      </div>
+     
     </div>
   );
 }
